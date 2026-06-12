fe203d2458a46895da7cc1e64991b7ae385ce29d
7TIN <prasadgujarathi619@gmail.com>
2026-06-12 20:30:36 +0530

    feat: initial setup

---
+++ apps/web/src/App.tsx
@@ -0,0 +1,15 @@
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+// import { APITester } from "./APITester";
+import "./index.css";
+
+
+
+export function App() {
+  return (
+    <div className="container mx-auto p-8 text-center relative z-10 font-mono">
+      Hello
+    </div>
+  );
+}
+
+export default App;
