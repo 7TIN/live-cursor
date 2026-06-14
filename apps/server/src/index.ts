348a28b338c40ffc16648d0b396505a3c77c8b95
7TIN <prasadgujarathi619@gmail.com>
2026-06-14 14:10:17 +0530

    feat: websocket connection with brodcasting

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,33 +1,75 @@
+// import type { WebSocket } from "bun";
+import type { ServerWebSocket } from "bun";
 import { Hono } from "hono";
-
+import { websocket } from "hono/bun";
 
 const app = new Hono();
 
 
-const webSocket = {
 
-    open(ws: WebSocket) {
-        console.log("connected");
-    },
 
-    close(ws: WebSocket) {
-        console.log("Closed")
-    }
+// type WebSocket = {
+//     send() : () => {}
+// }
 
+// const webSocket = {
+
+//     open(ws: WebSocket) {
+//         console.log("connected");
+//     },
+
+//     close(ws: WebSocket) {
+//         console.log("Closed")
+//     }
+
+// }
+
+interface websocketType extends ServerWebSocket {
+    connectionID?: string, 
 }
 
-
 app.get("/health", (c) => {
-    return c.json({
-        status : 200,
-        Message : "server is running",
-    })
-})
+  return c.json({
+    status: 200,
+    Message: "server is running",
+  });
+});
+
+const sockets : websocketType[] = [];
 
 Bun.serve({
-    fetch : app.fetch,
-    port : 3001,
-    // websocket : webSocket
-})
+  port: 3001,
+
+  fetch(req, server) {
+    const url = new URL(req.url);
+
+    if (url.pathname === "/ws") {
+      if (server.upgrade(req)) {
+        return;
+      }
+    }
+    return app.fetch(req);
+  },
+
+  // websocket : webSocket
+  websocket: {
+
+    open: (ws : websocketType) => {
+      console.log("connection open");
+      ws.connectionID = "2342sdsd"
+      sockets.push(ws);
+    },
+    close: (ws) => {
+      console.log("connection close");
+    },
+    message: (ws : websocketType, message) => {
+      console.log(message);
+      sockets.map((w) => {
+        w.send(message);
+        w.send(w.connectionID ? w.connectionID : "null")
+      })
+    },
+  },
+});
 
 console.log(`server is running on http://localhost:3001`);
